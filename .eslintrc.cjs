f0472f8c0cb8c6dd5d719a2af679dc51d166704f
Nasirdinov <nasirdinov.git@gmail.com>
2023-10-30 16:28:18 +0600

    disable eslint "react/no-array-index-key" rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react/display-name': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/no-array-index-key': 'off',
     'react/function-component-definition': 'off',
     'react/jsx-uses-react': 'off',
     'import/no-anonymous-default-export': 'off',
